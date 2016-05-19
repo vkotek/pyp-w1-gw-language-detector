f63be83f3e631e4dd0c492ffc81d1fe2e4a63502
Santiago Basulto <santiago.basulto@gmail.com>
2016-05-19 18:54:09 -0300

    Update main.py

--- language_detector/main.py
+++ language_detector/main.py
@@ -3,7 +3,7 @@
 from .languages import LANGUAGES
 
 
-def detect_language(text, languages):
+def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
     # implement your solution here
     pass
